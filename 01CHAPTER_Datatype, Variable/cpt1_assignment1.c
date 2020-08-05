0e432475d697f7d550ee5bde1e382dd04daade9d
martinyangcp <martinyangcp@gmail.com>
2020-08-05 14:36:34 +0900

    assignment

---
+++ 01CHAPTER_Datatype, Variable/cpt1_assignment1.c
@@ -0,0 +1,30 @@
+/*
+#include <stdio.h>
+
+int main() {
+	char place;
+	char winner;
+	char loser;
+	char ace;
+	char score;
+
+	printf("경기장은 어디입니까?\n");
+	scanf("%c", &place);
+	printf("이긴 팀은 어디입니까?\n");
+	scanf("%c", &winner);
+	printf("진 팀은 어디입니까?\n");
+	scanf("%c", &loser);
+	printf("우수 선수는 누구입니까?\n");
+	scanf("%c", &ace);
+	printf("스코어는 몇대 몇입니까?\n");
+	scanf("%c", &score);
+
+	printf("====================================================\n");
+	printf("오늘 %c 에서 야구 경기가 열렸습니다.\n",place);
+	printf("%c 과 %c, 은 치열한 공방전을 펼쳤습니다.\n",winner,loser);
+	printf("%c이 맹활약을 하였습니다.\n",ace);
+	printf("결국 %c이(가) %c을(를) %c로 이겼습니다.\n",winner,loser,score);
+	printf("====================================================");
+
+}
+*/
