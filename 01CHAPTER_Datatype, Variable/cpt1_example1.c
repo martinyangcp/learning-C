9a3b50b037e9a92498f9e5c4162134b81263432d
martinyangcp <martinyangcp@gmail.com>
2020-08-05 14:37:14 +0900

    example

---
+++ 01CHAPTER_Datatype, Variable/cpt1_example1.c
@@ -0,0 +1,16 @@
+/*
+#include <stdio.h>
+
+int main() {
+	double mit;
+	double nop;
+	double area;
+	
+	printf("밑변 높이 :\n");
+	scanf("%lf %lf", &mit, &nop);
+	area = (mit * nop) / 2;
+	printf("밑변 = %.2lfcm, 높이 = %.2lfcm인 삼각형의 면적 = %.2lfcm" ,mit,nop,area);
+
+
+}
+*/
