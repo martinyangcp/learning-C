0f67fdf0e06755c16dde993fa448f31b42aed67f
martinyangcp <martinyangcp@gmail.com>
2020-08-05 14:37:43 +0900

    example

---
+++ 01CHAPTER_Datatype, Variable/cpt1_example2.c
@@ -0,0 +1,18 @@
+/*
+#include <stdio.h>
+
+int main() {
+	int korean = 89;
+	int worldh = 100;
+	int math = 95;
+	double average = (korean + worldh + math) / 3.0;
+
+	printf("학번   :  20201234 \n");
+	printf("=======================\n");
+	printf("국어   :   %3d(점)\n", korean);
+	printf("세계사 :   %3d(점)\n", worldh);
+	printf("수학   :   %3d(점)\n", math);
+	printf("=======================\n");
+	printf("평균   :   %.2lf\n",average);
+}
+*/
