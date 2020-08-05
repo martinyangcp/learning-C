96691c434b76fab693cba4138884bdd1b860e597
martinyangcp <martinyangcp@gmail.com>
2020-08-05 14:38:01 +0900

    example

---
+++ 01CHAPTER_Datatype, Variable/cpt1_example3.c
@@ -0,0 +1,21 @@
+/*
+#include <stdio.h>
+
+int main() {
+	char gender;
+	int age;
+	double height;
+	printf("성별은? (남자는 M, 여자는 F) :\n ");
+	scanf("%c", &gender);
+	printf("나이는? :\n ");
+	scanf("%d", &age);
+	getchar();			// 버퍼의 내용을 지운다.
+	printf("키는? :\n\n ");
+	scanf("%lf", &height);
+	printf("===================\n");
+	printf("성별 : %c\n" , gender);
+	printf("나이 : %d\n" , age);
+	printf("키 : %lf\n" , height);
+
+}
+*/
