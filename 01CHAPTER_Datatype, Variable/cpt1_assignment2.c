8bf3ce93746a21e5311bc318963399c83720a9f5
martinyangcp <martinyangcp@gmail.com>
2020-08-05 14:36:57 +0900

    assignment

---
+++ 01CHAPTER_Datatype, Variable/cpt1_assignment2.c
@@ -0,0 +1,22 @@
+/*
+#include <stdio.h>
+
+int main() {
+	int firstnum;
+	int secondnum;
+	int thirdnum;
+	double avg;
+
+	printf("첫 번째 숫자를 입력하시오 :\n");
+	scanf("%d", &firstnum);
+	printf("두 번째 숫자를 입력하시오 :\n");
+	scanf("%d", &secondnum);
+	printf("세 번째 숫자를 입력하시오 :\n");
+	scanf("%d", &thirdnum);
+
+	avg = (firstnum + secondnum + thirdnum) / 3.0;
+	printf("%d%d%d 의 평균은 %.2lf 입니다.", firstnum, secondnum, thirdnum, avg);
+
+}
+
+*/
