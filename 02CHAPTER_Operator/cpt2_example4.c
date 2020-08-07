72011a3c1f4a0ae832dd2252e7a27f0583dc85c6
martinyangcp <martinyangcp@gmail.com>
2020-08-07 12:15:45 +0900

    example

---
+++ 02CHAPTER_Operator/cpt2_example4.c
@@ -0,0 +1,20 @@
+/*
+#include <stdio.h>
+int main() {
+
+	int jung;
+
+	int result = 0;
+	printf("정수를 입력하시오 :");
+	scanf("%d", &jung);
+	
+	result+= jung / 1000;
+	result += (jung % 1000)/ 100;
+	result += (jung % 100) / 10;
+	result += jung % 10;
+	
+	printf("자리 수의 합 : %d", result);
+
+
+}
+*/
