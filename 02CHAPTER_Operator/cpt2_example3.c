8dbbb5c64a20948def9c979f3536e11716ad0473
martinyangcp <martinyangcp@gmail.com>
2020-08-07 12:14:52 +0900

    example

---
+++ 02CHAPTER_Operator/cpt2_example3.c
@@ -0,0 +1,26 @@
+/*
+
+#include <stdio.h>
+int main() {
+	int x;
+	double y;
+	printf("x입력 : ");
+	scanf("%d", &x);
+
+	y = 2.0 * x * x * x - 3.0 * x * x + 1.0 / x;
+	printf("-> x =%d 일때	%lf", x, y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+*/
