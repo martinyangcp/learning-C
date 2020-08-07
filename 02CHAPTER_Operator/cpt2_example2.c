3d67229be5f432b60b87f777bd9a57cb6036f902
martinyangcp <martinyangcp@gmail.com>
2020-08-07 12:14:26 +0900

    example

---
+++ 02CHAPTER_Operator/cpt2_example2.c
@@ -0,0 +1,13 @@
+/*
+#include <stdio.h>
+int main(){
+	int month;
+	int wol;
+	printf("월(1~12) 입력 : ");
+	scanf("%d", &month);
+	month >6 ? printf("month월은 상반기입니다.") : printf("month월은 하반기입니다.");
+
+	
+	
+}
+*/
