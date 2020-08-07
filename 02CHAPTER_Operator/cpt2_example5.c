750cc0b46b3e43b0537c2f11f7fe2444ad2d73fb
martinyangcp <martinyangcp@gmail.com>
2020-08-07 12:16:29 +0900

    example

---
+++ 02CHAPTER_Operator/cpt2_example5.c
@@ -0,0 +1,35 @@
+/*
+#include <stdio.h>
+int main() {
+
+	int totalsec;
+	int min;
+	int hour;
+	int sec;
+
+	printf("초(sec) 입력 : ");
+	scanf("%d",&totalsec );
+
+	sec = totalsec % 60;
+	hour = (totalsec / 60) / 60;
+	min = (totalsec / 60)%60;
+
+	printf("입력한 %d초는 %d시간 %d분 %d초입니다.", totalsec, hour, min, sec);
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+*/
