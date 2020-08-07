7995a3920c24a0971250df07a930dfa6f08ebe58
martinyangcp <martinyangcp@gmail.com>
2020-08-07 12:13:59 +0900

    example

---
+++ 02CHAPTER_Operator/cpt2_example1.c
@@ -0,0 +1,18 @@
+/*
+#include <stdio.h>
+
+int main() {
+	int height;
+	double feet;
+	double inch;
+
+
+	printf("키(cm)를 입력하세요 :\n");
+	scanf("%d", &height);
+	inch = height / 2.54;
+	feet = inch * 12;
+
+	printf("입력하신 키 %d센티미터는 %.2lf피트와 %.2lf인치에 해당됩니다.", height, feet, inch);
+
+
+}*/
