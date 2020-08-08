af45a9b23066574ac81f4486e7d69911eb6cd032
martinyangcp <martinyangcp@gmail.com>
2020-08-08 11:28:24 +0900

    example

---
+++ 03CHAPTER_If,Swich,For,While,Break,Contine,Goto/cpt3_example1.c
@@ -0,0 +1,28 @@
+/*
+#include <stdio.h>
+
+int main() {
+	int temper;
+
+
+	printf("현재온도는?\n");
+	scanf("%d", &temper);
+
+	if (temper >= 0) {
+		printf("현재 온도는 %d도 입니다", temper);
+	}
+	else {
+		printf("오늘은 영하이며 현재 온도는 %d도 입니다.", temper);
+	}
+
+
+
+	
+
+
+
+
+
+
+}
+*/
