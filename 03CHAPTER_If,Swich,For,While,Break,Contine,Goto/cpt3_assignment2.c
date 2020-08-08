843fce531f974eb3d4c37a1d79267480f8adb85c
martinyangcp <martinyangcp@gmail.com>
2020-08-08 11:27:54 +0900

    assignment

---
+++ 03CHAPTER_If,Swich,For,While,Break,Contine,Goto/cpt3_assignment2.c
@@ -0,0 +1,46 @@
+
+/*
+#include <stdio.h>
+
+int main() {
+
+	int  st, i =0 ,sum =0;
+	
+	for (i = 0; i <= 4; i++) {
+		printf("양의정수 n을 입력하세요 >>\n");
+		scanf("%d", &st);
+		
+		if (st <= 0)
+		{
+			i -= 1;
+			continue;
+		}
+	
+
+		printf("지금까지의 합 %d +%d", sum, st);
+		sum += st;
+		printf("= %d\n", sum);
+
+
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	*/
