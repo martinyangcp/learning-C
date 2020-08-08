1a698ca7139fbcb475f10240e9aedea3e1d52e2c
martinyangcp <martinyangcp@gmail.com>
2020-08-08 11:27:31 +0900

    assignment

---
+++ 03CHAPTER_If,Swich,For,While,Break,Contine,Goto/cpt3_assignment1.c
@@ -0,0 +1,34 @@
+/*
+
+#include <stdio.h>
+
+int main() {
+	int num ,odd=0, even=0,k;
+
+	printf("정수 n 을 입력하시오 :");
+	scanf("%d", &num);
+
+	for (k = 1; k <= num; k += 2) {
+		odd += k;
+	}
+	for (k = 0; k <= num; k += 2) {
+		even += k;
+	}
+
+	printf("정수 1에서 %d이하 홀수들의 합은 %d이고 짝수들의 합은 %d입니다.", num, odd, even);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+*/
