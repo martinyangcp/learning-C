beea19b79ad77431d6f055ffae731aa8fbea4569
martinyangcp <martinyangcp@gmail.com>
2020-08-08 11:30:21 +0900

    example

---
+++ 03CHAPTER_If,Swich,For,While,Break,Contine,Goto/cpt3_example3.c
@@ -0,0 +1,32 @@
+/*
+#include <stdio.h>
+
+int main() {
+
+	int score;
+
+
+
+
+
+	printf("점수를 입력하시오 :");
+	scanf("%d", &score);
+
+	switch (score/10) {
+	case 10: 
+	case 9: printf("학점 :A"); break;
+	case 8: printf("학점 :B"); break;
+	case 7: printf("학점 :C"); break;
+	case 6: printf("학점 :D"); break;
+	default : printf("학점 :F"); break;
+	}
+	
+
+
+
+
+
+
+
+}
+*/
