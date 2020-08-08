6141cd78fabfb261fc42e1ded3b2bbb6f06b062b
martinyangcp <martinyangcp@gmail.com>
2020-08-08 11:28:50 +0900

    example

---
+++ 03CHAPTER_If,Swich,For,While,Break,Contine,Goto/cpt3_example2.c
@@ -0,0 +1,35 @@
+
+/*
+#include <stdio.h>
+
+int main() {
+	int num;
+
+	printf("정수를 입력하시오 :");
+	scanf("%d", &num);
+
+	if (num % 2 == 1) {
+
+		printf("입력한 수 %d는(은) 홀수입니다.", num);
+	}
+	else {
+		printf("입력한 수 %d는(은) 짝수입니다.", num);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+*/
