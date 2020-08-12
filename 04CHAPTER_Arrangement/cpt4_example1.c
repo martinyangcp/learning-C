688f27b105110657605a3c88feaa5b8b4c92f948
martinyangcp <martinyangcp@gmail.com>
2020-08-12 22:41:17 +0900

    example

---
+++ 04CHAPTER_Arrangement/cpt4_example1.c
@@ -0,0 +1,42 @@
+/*
+
+include <stdio.h>
+int main(){
+	int num, i;
+
+
+	int score[5] = { 0 };
+	for (i = 0; i < 5; i++) {
+		printf("%d번째 학생의 점수는 ?\n", i+1);
+		scanf("%d", &score[i]);
+
+	}
+
+	for (i = 0; i < 5; i++) {
+
+		printf("%d번째 학생 = %d",i+1 ,score[i]);
+		printf("점\n");
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+*/
