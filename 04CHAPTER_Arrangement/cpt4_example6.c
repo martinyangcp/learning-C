048b946dc1439f408afea423db331340e5f5f9e2
martinyangcp <martinyangcp@gmail.com>
2020-08-13 23:53:35 +0900

    example

---
+++ 04CHAPTER_Arrangement/cpt4_example6.c
@@ -0,0 +1,23 @@
+/*
+#include<stdio.h>
+int main() {
+	char school[10], name[10], major[10];
+
+	printf("학교는?\n");
+	scanf("%s", school);
+
+	printf("학과명은?\n");
+	scanf("%s", major);
+
+	printf("이름은?\n");
+	scanf("%s", name);
+	
+	printf("==========================\n");
+	printf("접속하신 분의 소속 학교는 %s 이며,", school);
+	printf("전공은 %s인 %s씨 입니다. \n", major, name);
+	printf("===========================");
+
+
+
+}
+*/
