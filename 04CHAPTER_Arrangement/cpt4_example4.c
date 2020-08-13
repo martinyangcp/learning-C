d045504179c150d3a8b683d6be8e4f09af813f8b
martinyangcp <martinyangcp@gmail.com>
2020-08-13 23:54:05 +0900

    example

---
+++ 04CHAPTER_Arrangement/cpt4_example4.c
@@ -0,0 +1,25 @@
+/*
+#include<stdio.h>
+int main() {
+	int i,j;
+
+
+	int matrix[2][3] = { { 0,0,0 }, { 0,0,0 } };
+
+	printf("행렬 A 입력\n");
+	for (i = 0; i < 2; i++) {
+
+		for (j = 0; j < 3; j++) {
+
+			printf("%d행 %d열?\n", i + 1,j+1);
+			scanf("%d", &matrix[i][j]);
+		}
+
+	}
+	printf("====================================\n");
+	printf("행렬 A");
+	printf(matrix[2][3]);
+
+
+}
+*/
