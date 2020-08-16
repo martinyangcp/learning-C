1d76e1e2c79585d46e86bb9adfbcf4fe4f98d206
martinyangcp <martinyangcp@gmail.com>
2020-08-16 22:18:34 +0900

    example

---
+++ 05CHAPTER_Function/cpt5_example1.c
@@ -0,0 +1,28 @@
+/*
+
+#include <stdio.h>
+
+int compa(int A, int B);			// A,B 중 큰 값을 반환함
+
+int main() {
+	int fir, sec;
+
+	printf("첫째 정수?");
+	scanf("%d", &fir);
+	printf("둘째 정수?");
+	scanf("%d", &sec);
+	printf("%d, %d 중 큰 값은%d",fir,sec, compa(fir,sec));
+
+}
+
+int compa(int A, int B) {
+	
+	if (A > B) {
+		return A;
+	}
+	else {
+		return B;
+	}
+}
+
+*/
