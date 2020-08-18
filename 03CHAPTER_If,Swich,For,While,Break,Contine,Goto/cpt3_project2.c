67c3676764266a574c3668a103c5202e44c5b081
martinyangcp <martinyangcp@gmail.com>
2020-08-18 23:57:34 +0900

    project

---
+++ 03CHAPTER_If,Swich,For,While,Break,Contine,Goto/cpt3_project2.c
@@ -0,0 +1,49 @@
+/*
+#include <stdio.h>
+int main() {
+	int exam, score, count, num;
+	
+	here :
+	printf("시험과 과제성적 입력해라");
+	scanf("%d" "%d", &exam, &score);
+	num = (exam + score) / 2;
+
+	
+
+	if (0 <= num <= 40) {			
+		printf("F");
+	}
+	else if (41 <= num <= 50) {
+		printf("C");
+	}
+	else if (51 <= num <= 60) {
+		printf("C+");
+	}
+	else if (61 <= num <= 70) {
+		printf("B");
+	}
+	else if (71 <= num <= 80) {
+		printf("B+");
+	}
+	else if (81 <= num <= 90) {
+		printf("A");
+	}
+	else if (91 <= num <= 100) {
+		printf("A+");
+	}
+	else {
+		count += 1;
+		if (count < 3) {
+			printf("성적은 0~100 사이 정수로 입력하세요!");
+			goto here;
+		}
+		else if (count == 3) {
+			printf("입력된 값 오류로 프로그램을 종료합니다");
+
+		}
+
+
+	}
+
+}
+*/
