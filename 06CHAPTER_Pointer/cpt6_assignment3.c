19891031cb649babcd2f00e46c44763a773cef59
martinyangcp <martinyangcp@gmail.com>
2020-08-24 23:59:43 +0900

    assignment

---
+++ 06CHAPTER_Pointer/cpt6_assignment3.c
@@ -0,0 +1,22 @@
+/*
+#include<stdio.h>
+int main() {
+	int arr[] = { 34, -67, 25, 43, -69, 29 };
+	int i, j, min, max, minor, maxor;
+
+	min = *(arr + 0);
+	max = *(arr + 0);
+	for (i = 0; i < 6; i++) {
+		if (*(arr + i) >= max) {
+			max = *(arr + i);
+			maxor = i + 1;
+		}
+		if (*(arr + i) <= min) {
+			min = *(arr + i);
+			minor = i + 1;
+		}
+	}
+	printf("ÃÖ´ñ°ª : %d , À§Ä¡ : %d\n", max, maxor);
+	printf("ÃÖ¼Ú°ª : %d , À§Ä¡ : %d", min, minor);
+}
+*/
