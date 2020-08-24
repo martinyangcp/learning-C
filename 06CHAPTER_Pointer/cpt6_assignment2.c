0d5bdfcb1c1d87b1db3cbd2ed9f23dbfa94e3a75
martinyangcp <martinyangcp@gmail.com>
2020-08-24 23:59:16 +0900

    assignment

---
+++ 06CHAPTER_Pointer/cpt6_assignment2.c
@@ -0,0 +1,15 @@
+/*
+#include<stdio.h>
+int main() {
+	int arr[10] = { 5, 8, 3 };
+	int i,result=1;
+	for (i = 0; i < 3; i++) {
+		printf("arr[%d] = %d\n",i, *(arr + i));
+		result *=  *(arr + i);
+	}
+	;
+	printf("arr[0] * arr[1] * arr[2] = %d",result);
+
+
+}
+*/
