b3dc494a50254e8f61ef661d6ce9286babe5fb49
martinyangcp <martinyangcp@gmail.com>
2020-08-26 14:36:16 +0900

    example

---
+++ 03CHAPTER_If,Swich,For,While,Break,Contine,Goto/cpt3_example4.c
@@ -0,0 +1,29 @@
+/*
+
+#include <stdio.h>
+
+int main() {
+	int sum = 0, i, n;
+
+	printf("N값?");
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		sum += i;
+		printf("1~%d까지의 합 : %d  \n",i,sum);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+*/
