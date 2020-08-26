5285ac8444688a794eff055f8e2789bd1f082ba3
martinyangcp <martinyangcp@gmail.com>
2020-08-26 14:37:26 +0900

    example

---
+++ 04CHAPTER_Arrangement/cpt4_example2.c
@@ -0,0 +1,24 @@
+/*
+#include <stdio.h>
+int main() {
+	int num, i;
+	double avg=0;
+
+	int score[5] = { 0 };
+	for (i = 0; i < 5; i++) {
+		printf("%d번째 학생의 점수는 ?\n", i + 1);
+		scanf("%d", &score[i]);
+
+	}
+	for (i = 0; i < 5; i++) {
+		avg += score[i];
+	}
+	avg = avg / 5.0;
+
+	printf("평균 :");
+	printf("%lf점", avg);
+
+
+
+}
+*/
