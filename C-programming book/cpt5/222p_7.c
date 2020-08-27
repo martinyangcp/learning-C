b49982dc05b0313b898a1332cc9f21c4026616e5
martinyangcp <martinyangcp@gmail.com>
2020-08-27 23:56:52 +0900

    book

---
+++ C-programming book/cpt5/222p_7.c
@@ -0,0 +1,26 @@
+
+/*
+#include <stdio.h>
+int main() {
+	double weight, height, standard;
+
+	printf("키 :");
+	scanf("%lf", &height);
+	printf("체중 :");
+	scanf("%lf", &weight);
+	standard = (height - 100) * 0.9;
+
+	if (weight > standard + 5) {
+		printf("당신은 과체중입니다.");
+	}
+	else if (weight < standard - 5) {
+		printf("당신은 저체중입니다.");
+	}
+	else {
+		printf("당신은 표준체중 입니다.");
+	}
+
+
+
+}
+*/
