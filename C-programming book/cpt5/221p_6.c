efb5e9324ee8aa0b39af9d9d5ec7bfd3f9413df7
martinyangcp <martinyangcp@gmail.com>
2020-08-27 23:56:20 +0900

    book

---
+++ C-programming book/cpt5/221p_6.c
@@ -0,0 +1,31 @@
+/*
+
+
+#include <stdio.h>
+int main() {
+	
+	char mun, start;
+	int i,startt ;
+
+	printf("문자 입력 :");
+	scanf("%c", &mun);
+	i = (int)mun;
+	for (start = 'a'; start < 'z'; start++) {
+		if (start == mun) {
+			printf("알파벳소문자 입니다.");
+		}
+	}
+	for (start = 'A'; start < 'Z'; start++) {
+		if (start == mun) {
+			printf("알파벳대문자 입니다.");
+		}
+	}
+	for  (startt = 0 ; startt < 10; startt++) {
+		if (startt == i) {
+			printf("숫자 입니다.");
+		}
+	}
+
+
+}
+*/
