5b36298cca6b40214c1c429e6efe9689d53292da
martinyangcp <martinyangcp@gmail.com>
2020-08-27 23:57:17 +0900

    book

---
+++ C-programming book/cpt5/222p_8.c
@@ -0,0 +1,37 @@
+/*
+#include <stdio.h>
+int main() {
+	int month;
+
+	printf("월 입력 :");
+	scanf("%d\n", &month);
+	printf(">>");
+
+	switch (month) {
+
+	case 1:
+	case 2:
+	case 3:
+	printf("1사분기 입니다.");
+	break;
+	case 4: 
+	case 5: 
+	case 6:
+	printf("2사분기 입니다.");
+	break;
+	case 7:
+	case 8: 
+	case 9:
+	printf("3사분기 입니다.");
+	break;
+	case 10:
+	case 11:
+	case 12: 
+	printf("4사분기 입니다.");
+	break;
+	}
+	return 0;
+	
+
+}
+*/
