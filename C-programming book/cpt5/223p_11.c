012290a5d1f4dea62cad9460fcfd5763859afce1
martinyangcp <martinyangcp@gmail.com>
2020-08-28 23:53:31 +0900

    book

---
+++ C-programming book/cpt5/223p_11.c
@@ -0,0 +1,30 @@
+
+/*
+#include <stdio.h>
+int main() {
+
+	int jung,detersosu=0,i,help;
+
+	printf("양의 정수 n :");
+	scanf("%d", &jung);
+
+	for (i = 2; i < jung; i++) {
+
+		help = jung % i;
+		if (help == 0) {
+			detersosu += 1;
+		}
+
+	}
+	if (detersosu > 0) {
+		printf("%d는(은) 소수가 아닙니다.", jung);
+
+	}
+	else {
+		printf("%d는(은) 소수입니다.", jung);
+	}
+
+	return 0;
+
+}
+*/
