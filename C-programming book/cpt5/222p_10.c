9e20a6c33be20b753a564bc51c0689ece9462bf3
martinyangcp <martinyangcp@gmail.com>
2020-08-28 23:52:45 +0900

    book

---
+++ C-programming book/cpt5/222p_10.c
@@ -0,0 +1,33 @@
+
+/*
+#include <stdio.h>
+int main() {
+	int num, answer,way;
+
+	printf("수 입력 :");
+	scanf("%d", &num);
+
+
+	printf("계산법(1,2,3) :");
+	scanf("%d", &way);
+
+	switch (way) {
+
+	case 1:
+		answer = 1;
+		printf("입력한수 %d의 %d배수는 %d입니다.", num, answer, num * answer);
+		break;
+	case 2:
+		answer = 2;
+		printf("입력한수 %d의 %d배수는 %d입니다.", num, answer, num * answer);
+		break;
+	case 3:
+		answer = 3;
+		printf("입력한수 %d의 %d배수는 %d입니다.", num, answer, num * answer);
+		break;
+
+	default: printf("잘못된 연산자 입니다.");
+	}
+	return 0;
+
+}*/
