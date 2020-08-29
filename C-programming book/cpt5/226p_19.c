ebde9b09031e036faca679614e339c0cbc309d78
martinyangcp <martinyangcp@gmail.com>
2020-08-29 23:49:40 +0900

    book

---
+++ C-programming book/cpt5/226p_19.c
@@ -0,0 +1,20 @@
+
+
+/*
+#include <stdio.h>
+int main() {
+
+	int dan = 0, i = 0;
+
+	for (i = 1; i <= 9; i++) {
+		for (dan = 2; dan <= 9; dan++)
+			printf(" %d*%d = %d ", dan, i, dan * i);
+
+	}
+
+
+
+
+
+}
+*/
