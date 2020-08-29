3bf56ea1626699fef77f10d032264b200078781a
martinyangcp <martinyangcp@gmail.com>
2020-08-29 23:50:04 +0900

    book

---
+++ C-programming book/cpt5/226p_20.c
@@ -0,0 +1,20 @@
+
+/*
+
+#include <stdio.h>
+int main() {
+
+	int i=1, n,sum;
+	sum = 1;
+	printf("숫자를 입력하세요 :");
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		sum *= i;
+
+	}
+	printf("%d! = %d",n, sum);
+
+
+
+}
+*/
