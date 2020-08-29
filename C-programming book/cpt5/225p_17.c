6f1da08039a1593087bd55a7a68f1692f8345ef5
martinyangcp <martinyangcp@gmail.com>
2020-08-29 23:49:04 +0900

    book

---
+++ C-programming book/cpt5/225p_17.c
@@ -0,0 +1,20 @@
+/*
+
+#include <stdio.h>
+int main() {
+
+	int i,n , sum=0;
+
+	printf("제곱의 합을 구할n을 입력 :");
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		sum += i * i;
+
+	}
+		printf("1부터 %d까지의 제곱의 합은 %d입니다.", n, sum);
+	
+
+
+}
+*/
