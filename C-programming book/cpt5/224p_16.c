eacef1301a4932be9db11fc11307d40db05296b3
martinyangcp <martinyangcp@gmail.com>
2020-08-29 23:48:39 +0900

    book

---
+++ C-programming book/cpt5/224p_16.c
@@ -0,0 +1,22 @@
+
+/*
+#include <stdio.h>
+int main() {
+
+	int num=0,sum=0,formernum;
+
+	formernum = 1;
+	
+	while (1) {
+		sum += formernum;
+		formernum++;
+		num++;
+		if (sum > 1000)
+			break;
+	}
+
+
+	printf("1부터 %d까지의 합이 %d입니다.", num - 1, sum - num);
+
+}
+*/
