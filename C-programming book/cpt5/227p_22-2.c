ca9f3e550284558261b92e60b6f02398634fd173
martinyangcp <martinyangcp@gmail.com>
2020-08-29 23:50:58 +0900

    book

---
+++ C-programming book/cpt5/227p_22-2.c
@@ -0,0 +1,44 @@
+/*
+#include <stdio.h>
+int main() {
+	int c = 0, i = 4, k = 1, a = 0;
+
+
+	for (c = 1; c < 6; c++) {
+
+		for (a = 0; a < i; a++) {
+			printf(" ");
+		}
+
+		for (a = 0; a < k; a++) {
+			printf("*");
+		}
+
+		printf("\n");
+		i -= 1;
+		k += 2;
+	}
+
+	i += 2;
+	k -= 4;
+
+
+	for (c = 1; c < 7; c++) {
+		
+		for (a = 0; a < i; a++) {
+			printf(" ");
+		}
+
+		for (a = 0; a < k; a++) {
+			printf("*");
+		}
+
+		printf("\n");
+		i += 1;
+		k -= 2;
+	}
+
+
+
+}
+*/
