43d7c6d372f6d4d4682ff70849a176c707ff13c5
martinyangcp <martinyangcp@gmail.com>
2020-08-29 23:50:16 +0900

    book

---
+++ C-programming book/cpt5/226p_21.c
@@ -0,0 +1,21 @@
+/*
+
+#include <stdio.h>
+int main() {
+	int b, i, a;
+
+	printf("두개의 정수 입력 :");
+	scanf("%d %d", &a, &b);
+	printf("%d이지만 %d의 배수는 아닌 수", a, b);
+	for (i = 1; i < 101; i++) {
+		
+		if ((i % a) == 0 && (i % b) != 0) {
+			printf("%d ", i);
+		}
+
+	}
+
+
+
+}
+*/
