c3a15e175504e418f29df367162c7a109c6be2c5
martinyangcp <martinyangcp@gmail.com>
2020-08-29 23:51:16 +0900

    book

---
+++ C-programming book/cpt5/227p_22-3.c
@@ -0,0 +1,19 @@
+
+/*
+
+#include <stdio.h>
+int main() {
+
+	int i, k, p;
+
+	for (i = 1; i < 10; i++) {
+		for (k = 1; k < i + 1; k++) {
+			printf("%d", k);
+
+
+		}
+		printf("\n");
+	}
+}
+
+*/
