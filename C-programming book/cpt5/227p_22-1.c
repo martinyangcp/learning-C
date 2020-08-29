8dbe22d0a48803485bd3df3bfde9f49fc7a03584
martinyangcp <martinyangcp@gmail.com>
2020-08-29 23:50:44 +0900

    book

---
+++ C-programming book/cpt5/227p_22-1.c
@@ -0,0 +1,20 @@
+
+/*
+#include <stdio.h>
+int main() {
+	int i=0;
+	for (i = 0; i < 8; i++) {
+		printf("*");
+	}
+	printf("\n");
+	for (i = 0; i < 8; i++) {
+		printf("\n*      *\n");
+	}
+
+	printf("\n");
+	for (i = 0; i < 8; i++) {
+		printf("*");
+	}
+
+}
+*/
