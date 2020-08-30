15b7f8d40b324682686f881e29df14518d76eced
martinyangcp <martinyangcp@gmail.com>
2020-08-30 23:57:09 +0900

    book

---
+++ C-programming book/cpt5/228p-24.c
@@ -0,0 +1,41 @@
+
+
+/*
+
+#include <stdio.h>
+int main() {
+
+	int a, b, x, y, r;
+
+	printf("두 개의 정수를 입력하시오 :");
+	scanf("%d %d", &a, &b);
+
+	if (a >= b) {
+		x = a;
+		y = b;
+	}
+	else {
+		x = b;
+		y = a;
+	}
+
+	if (y == 0) {
+		goto go;
+	}
+
+	
+		while (1)
+		{r = x % y;
+		x = y;
+		y = r;
+		if (y == 0) {
+			goto go;
+
+		}
+	}
+go:
+
+	printf("최대공약수는 %d입니다", x);
+
+
+}*/
