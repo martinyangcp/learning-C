d96e74d6976731667f05fb09473bd7e4b59428bd
martinyangcp <martinyangcp@gmail.com>
2020-08-30 23:57:46 +0900

    book

---
+++ C-programming book/cpt6/250p_7.c
@@ -0,0 +1,16 @@
+/*
+#include <stdio.h>
+#define SQ(x) ((x)*(x))
+int main() {
+	int n = 0, i = 0,sum=0;
+
+	printf("n의 값을 입력하세요 :");
+	scanf("%d", &n);
+	for (i = 1; i < n+1; i++) {
+		printf("%d+", SQ(i));
+		sum += SQ(i);
+	}
+	printf("=%d", sum);
+
+}
+*/
