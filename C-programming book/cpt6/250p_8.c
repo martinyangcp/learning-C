fce50db4f793383539279fa7a70422f2292ecd1d
martinyangcp <martinyangcp@gmail.com>
2020-08-30 23:57:59 +0900

    book

---
+++ C-programming book/cpt6/250p_8.c
@@ -0,0 +1,14 @@
+
+/*
+#include<stdio.h>
+#define PIE 3.141592
+#define VOLUME(r) ((4*PIE*(r)*(r)*(r))/3)
+int main() {
+	double r;
+	printf("반지름 :");
+	scanf("%lf", &r);
+	printf("반지름이 %lf인 구의 부피 =%d", r, VOLUME(r));
+	
+}
+
+*/
