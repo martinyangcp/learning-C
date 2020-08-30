1709c643c7fd4cb1f374cf0f4bb7eaad98fa0f76
martinyangcp <martinyangcp@gmail.com>
2020-08-30 23:57:27 +0900

    book

---
+++ C-programming book/cpt6/249p_6.c
@@ -0,0 +1,16 @@
+/*
+#include<stdio.h>
+#define MAX(a,b,c) a<b?(b<c?c:b):(a<c?c:a)
+
+int main() {
+	int x, y, z;
+	printf("세개의 수");
+	scanf("%d %d %d", &x, &y, &z);
+	
+	printf(">>가장 큰 수 :%d",MAX(x,y,z));
+	
+
+
+
+}
+*/
