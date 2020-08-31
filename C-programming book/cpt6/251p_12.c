3315f5710caa7ad4ad587eaa4455be73cf4ef556
martinyangcp <martinyangcp@gmail.com>
2020-08-31 22:45:10 +0900

    book

---
+++ C-programming book/cpt6/251p_12.c
@@ -0,0 +1,24 @@
+/*
+#include<stdio.h>
+#define NUMBER(x) printf("%d	%o	%x\n",x, x ,x);
+int main() {
+	int i,k;
+	i = 15;
+	k = 0;
+
+	printf("-------------------------------\n");
+	printf("8진수	10진수	16진수\n");
+	printf("-------------------------------\n");
+		for (k = 0; k < i; k++) {
+
+			NUMBER(k);
+
+
+	}
+
+	printf("-------------------------------\n");
+
+
+
+
+}*/
