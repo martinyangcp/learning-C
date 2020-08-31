2a13516ac03c06969c9af52ba7c4049e7de5ca79
martinyangcp <martinyangcp@gmail.com>
2020-08-31 22:44:56 +0900

    book

---
+++ C-programming book/cpt6/251p_11.c
@@ -0,0 +1,14 @@
+/*
+#include <stdio.h>
+#define NUMBER(x) printf("%d %o %x",x, x ,x);
+int main() {
+
+	int num=0;
+
+	printf("정수 입력 :");
+	scanf("%d", &num);
+	printf(">>결과 8진수 10진수 16진수");
+	NUMBER(num);
+
+}
+*/
