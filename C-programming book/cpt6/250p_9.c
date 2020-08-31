2b82d51687b92096b4a5d773280fac0253bf7986
martinyangcp <martinyangcp@gmail.com>
2020-08-31 22:44:23 +0900

    book

---
+++ C-programming book/cpt6/250p_9.c
@@ -0,0 +1,14 @@
+
+/*
+#include<stdio.h>
+#define SWAP(x,y,t) ((t)=(x),(x)=(y),(y)=(t))
+int main() {
+	int a, b,t;
+	printf("두 수 입력 :");
+	scanf("%d %d", &a, &b);
+	SWAP(a, b, t);
+	printf("%d %d",a,b );
+
+
+}
+*/
