16341b47661f32dfd1651d0d36e1ff23c4edc6c5
martinyangcp <martinyangcp@gmail.com>
2020-09-01 22:31:32 +0900

    book

---
+++ C-programming book/cpt7/301p_5.c
@@ -0,0 +1,17 @@
+/*
+#include<stdio.h>
+
+int main() {
+
+	int days[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+	int total=0,n=0,i=0;
+
+	printf("1월부터 몇 월까지의 총일을 구할까요?");
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++)
+	{total += days[i];}
+	printf("1월부터 %d월까지는 총 %d일이 있습니다.",n, total);
+
+
+}*/
