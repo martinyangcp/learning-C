83841086f1e3b78a08fd429b185490b75a8531b6
martinyangcp <martinyangcp@gmail.com>
2020-09-01 22:31:45 +0900

    book

---
+++ C-programming book/cpt7/301p_6.c
@@ -0,0 +1,17 @@
+
+/*
+#include<stdio.h>
+
+int main() {
+
+	int days[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+	int total = 0, n = 0, i = 0;
+
+	printf("12달 중 30일까지 있는 달은 ");
+	for (i = 1; i <13; i++)
+	{
+		if (days[i] == 30)total += 1;
+		if (days[i] == 30)printf("%d, ", i);
+	}
+	printf("총 %d달 입니다.", total);
+}*/
