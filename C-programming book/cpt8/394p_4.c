987f41eed4b5234eaf5089f9f3d5fd960e542419
martinyangcp <martinyangcp@gmail.com>
2020-09-03 23:56:48 +0900

    book

---
+++ C-programming book/cpt8/394p_4.c
@@ -0,0 +1,23 @@
+
+/*
+#include<stdio.h>
+double cm_to_feet(int height);
+int main() {
+
+	double height;
+
+	printf("당신의 키는(cm)?");
+	scanf("%lf", &height);
+
+	printf("키%.1lf cm는 %.1lf feet입니다.", height, cm_to_feet(height));
+
+	return 0;
+
+}
+
+double cm_to_feet(int height)
+{
+	double feet=0;
+	feet = height / 30.48;
+	return feet;
+}*/
