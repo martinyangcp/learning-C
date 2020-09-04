edcd62954deb089fe40767f70aed29ec75c4d5ef
martinyangcp <martinyangcp@gmail.com>
2020-09-05 02:54:05 +0900

    book

---
+++ C-programming book/cpt8/398p_11.c
@@ -0,0 +1,24 @@
+/*
+#include<stdio.h>
+
+void palindrome(int n);
+
+int main() {
+
+	int n;
+	printf("양의 정수 n을 입력하면 거꾸로 출력하는 프로그램입니다. n은?");
+	scanf("%d", &n);
+
+	palindrome(n);
+
+	return 0;
+}
+
+void palindrome(int n) {
+	while (n > 10) {
+		printf("%d", n % 10);
+		n = n / 10;
+	}
+	printf("%d", n);
+}
+*/
