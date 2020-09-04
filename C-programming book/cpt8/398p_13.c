d544e6e21eb1df6910685b96bae236e7d543c518
martinyangcp <martinyangcp@gmail.com>
2020-09-05 02:54:32 +0900

    book

---
+++ C-programming book/cpt8/398p_13.c
@@ -0,0 +1,23 @@
+
+/*
+#include<stdio.h>
+
+void palindrome(int n);
+
+int main() {
+
+	int n;
+	printf("양의 정수 n을 입력하면 2진수로 입력하는 프로그램입니다. n은?");
+	scanf("%d", &n);
+
+	palindrome(n);
+	return 0;
+}
+
+void palindrome(int n) {
+	while (n > 1) {
+		printf("1");
+		n = n / 2;
+	}
+	printf("%d",n%2);
+}*/
